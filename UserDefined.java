f47a07221c37d4d9cbbcb3e7027af18ee9545164
Amar Pawar <156612323+Amarpawar7@users.noreply.github.com>
2026-09-01 00:43:33 +0530

    Add files via upload

---
+++ UserDefined.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class AgeInvalid extends Exception{
+    public AgeInvalid(String str)
+    {
+        super(str);
+    }
+}
+
+class UserDefined {
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        
+        System.out.println("Enter your age : ");
+        int Age = sobj.nextInt();
+
+        try
+        {
+            if(Age<18)
+            {
+                throw new AgeInvalid("You are underage");
+            }
+            else{
+                System.out.println("You can attend the session");
+            }
+        }
+        catch(AgeInvalid aobj)
+        {
+            System.out.println("Inside Catah : "+aobj);
+        }
+        
+    }
+}
