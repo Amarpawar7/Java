1fc11486bde631dd49bdbfc288b8a54b2768c731
Amar Pawar <156612323+Amarpawar7@users.noreply.github.com>
2026-09-06 01:50:31 +0530

    Add files via upload

---
+++ abstract_demo_complete.java
@@ -0,0 +1,29 @@
+abstract class Demo{
+    public int i,j;
+
+    public int Add(int a, int b){           //  Concrete method
+        return a+b;
+    }
+    public abstract int Sub(int a, int b) ; 
+
+}
+
+class Hello extends Demo{           // ERROR - Hello is not abstract and does not override abstract method Sub(int,int) in Demo
+    public int Sub(int a, int b){
+        return a-b;
+    }   
+}
+
+class abstract_demo_complete{
+    public static void main(String A[]) {
+
+        Hello hobj = new Hello();
+
+        System.out.println(hobj.Add(10,11));
+        System.out.println(hobj.Sub(10,11));
+
+        
+
+    }
+}
+
