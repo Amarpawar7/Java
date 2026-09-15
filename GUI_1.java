5b54985756138f8281df85b2ddb28a1296128736
Amar Pawar <156612323+Amarpawar7@users.noreply.github.com>
2026-09-16 01:32:58 +0530

    Add files via upload

---
+++ GUI_1.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+class GUI_1
+{
+    public static void main(String A[])
+    {
+        JFrame fobj = new JFrame("Demo Frame");
+        
+        fobj.setSize(400,300);
+
+        fobj.setVisible(true);
+
+    }
+}
