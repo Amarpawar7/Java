4c0d049845dc137440f063cb627bfcf2c21f2305
Amar Pawar <156612323+Amarpawar7@users.noreply.github.com>
2026-09-16 01:33:11 +0530

    Add files via upload

---
+++ GUI_2.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+class GUI_2
+{
+    public static void main(String A[])
+    {
+        Frame fobj = new Frame("Demo Frame");
+        
+        fobj.setSize(400,300);
+
+        fobj.setVisible(true);
+
+    }
+}
