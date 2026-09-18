a5385217285d153b5669f4e2971f4a8e035a1507
Amar Pawar <156612323+Amarpawar7@users.noreply.github.com>
2026-09-19 00:18:12 +0530

    Add files via upload

---
+++ GUIButtonListner_1.java
@@ -0,0 +1,34 @@
+import java.awt.event.*;
+import javax.swing.*;
+
+class GUIButtonListner_1
+{
+    public static void main(String A[])
+    {
+        JFrame fobj = new JFrame("Demo Frame");
+        
+        JButton bobj = new JButton("OK");
+
+        bobj.setBounds(100,100,150,50);
+
+        bobj.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent aobj)
+            {
+                System.out.println("Button Clicked");
+            }
+        });
+        
+        fobj.add(bobj);
+
+        fobj.setSize(400,300);
+
+        fobj.setLayout(null);
+
+        fobj.setVisible(true);
+
+        fobj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+    }
+}
+
