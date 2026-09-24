9f9a37de44f7a9e5024093b7b72d691a1e61c351
Amar Pawar <156612323+Amarpawar7@users.noreply.github.com>
2026-09-24 10:48:11 +0530

    Add files via upload

---
+++ GUILoginForm_1.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+class GUILoginForm_1 extends JFrame
+{
+
+    public GUILoginForm()
+    {
+        setTitle("Demo Login Form");
+        setSize(400,300);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }
+
+    public static void main(String A[])
+    {
+        new GUILoginForm();
+
+    }
+}
+
